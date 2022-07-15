8580e884e636fdce14b8863a7e39da0fb7085da2
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2022-07-15 14:01:16 +0600

    Adds higher lower urls

--- server.py
+++ server.py
@@ -1,6 +1,11 @@
+import random
+
 from flask import Flask
 
 app = Flask(__name__)
+high_gif_link = "https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif"
+low_gif_link = "https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif"
+correct_gif_link = "https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif"
 
 
 def home_decorator(function):
@@ -11,10 +16,36 @@ def home_decorator(function):
     return wrapper
 
 
+def high_low_decorator(function):
+    def wrapper_fun(*args, **kwargs):
+        n = kwargs.get('num')
+        msg = function(n)
+        if n < random_number:
+            return f'<h1 style="color:blue;">{msg}</h1><img src={low_gif_link}>'
+        elif n > random_number:
+            return f'<h1 style="color:red;">{msg}</h1><img src={high_gif_link}>'
+        else:
+            return f'<h1 style="color:green;">{msg}</h1><img src={correct_gif_link}>'
+    return wrapper_fun
+
+
 @app.route("/")
 @home_decorator
 def home():
+    global random_number
+    random_number = random.randint(0, 9)
     return "Guess a number between 0 and 9"
+
+
+@app.route("/<int:num>")
+@high_low_decorator
+def higher_lower(num):
+    if num < random_number:
+        return "Too low, try again!"
+    elif num > random_number:
+        return "Too high, try again!"
+    else:
+        return "You found me!"
 
 
 if __name__ == "__main__":
